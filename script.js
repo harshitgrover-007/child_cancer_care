1066abacad9ce877ca1ed27353a4e1085626c1fd
Harshit Grover <74338878+harshitgrover-007@users.noreply.github.com>
2023-07-15 10:49:16 +0000

    change

--- script.js
+++ script.js
@@ -8,7 +8,23 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       });
     });
   });
-
+  
+  function adjustTextSize() {
+    const textContainer = document.querySelector('.text');
+    const circle = document.querySelector('.circle');
+    const circleWidth = circle.offsetWidth;
+    const textWidth = textContainer.offsetWidth;
+  
+    if (textWidth > circleWidth) {
+      const scaleFactor = circleWidth / textWidth;
+      textContainer.style.transform = `rotate(-90deg) scale(${scaleFactor})`;
+    }
+  }
+  
+  window.addEventListener('resize', adjustTextSize);
+  adjustTextSize();
+  
+  
   
   
 
