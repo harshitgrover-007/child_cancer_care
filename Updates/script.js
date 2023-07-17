9a0581402045206774d855d30c6ed32120df13a0
Harshit Grover <74338878+harshitgrover-007@users.noreply.github.com>
2023-07-17 04:05:58 +0000

    change

---
+++ Updates/script.js
@@ -0,0 +1,32 @@
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+
+
+document.onkeydown = (e) => {
+  if (
+    event.keyCode === 123 ||
+    ctrlShiftKey(e, 'I') ||
+    ctrlShiftKey(e, 'J') ||
+    ctrlShiftKey(e, 'C') ||
+    (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+  )
+    return false;
+};
+
+
+
+
+  
+  
+  
+  
+
+  
