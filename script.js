04dc365b62d37206ab1b127e1b1c72992c9d77d6
Harshit Grover <74338878+harshitgrover-007@users.noreply.github.com>
2023-07-17 06:15:30 +0000

    Update

--- script.js
+++ script.js
@@ -21,6 +21,24 @@ document.onkeydown = (e) => {
     return false;
 };
 
+window.addEventListener("load", function(){
+  setTimeout(
+      function open(event){
+          document.querySelector(".popup").style.display = "block";
+      },
+      5000
+  )
+});
+
+
+document.querySelector("#close").addEventListener("click", function(){
+  document.querySelector(".popup").style.display = "none";
+});
+
+document.getElementById("rounded").onclick = function() {
+  document.getElementById("pop").style.display = "none";
+}
+
 
 
 
